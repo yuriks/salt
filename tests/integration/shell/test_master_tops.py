83b8ce0d2e3951b81c2b1dd4359343c325b946cc
Pedro Algarvio <pedro@algarvio.me>
2020-03-11 12:37:03 +0000

    Switch from `TestCase` test into a plain pytest case

--- tests/integration/shell/test_master_tops.py
+++ tests/integration/shell/test_master_tops.py
@@ -7,21 +7,12 @@
 # Import Python libs
 from __future__ import absolute_import, print_function, unicode_literals
 
-# Import Salt Testing libs
-from tests.support.case import ShellCase
-
 import pytest
 
 
 @pytest.mark.windows_whitelisted
-class MasterTopsTest(ShellCase):
-
-    _call_binary_ = 'salt'
-
-    def test_custom_tops_gets_utilized(self):
-        resp = self.run_call(
-            'state.show_top'
-        )
-        self.assertTrue(
-            any('master_tops_test' in _x for _x in resp)
-        )
+def test_custom_tops_gets_utilized(salt_call_cli):
+    ret = salt_call_cli.run('state.show_top')
+    assert ret.exitcode == 0
+    assert 'master_tops_test' in ret.stdout
+    assert ret.json == {"base": ["core", "master_tops_test"]}
